11eefa67a5a2563bd73edc237bf385197feb9569
Urist McDwarf <umcdwarf@gmail.com>
2018-03-13 17:11:02 +0500

    Setup server

---
+++ app/assets/js/main.js
@@ -0,0 +1,103 @@
+$(function () {
+  // Анимация якоря
+  $(document).ready(function () {
+    $("#link").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('body,html').animate({ scrollTop: top }, 1000);
+    });
+  });
+
+  // Боковое меню
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 450,
+    'tolerance': 70,
+  });
+
+  document.querySelector('.menu-open').addEventListener('click', function () {
+    slideout.open();
+  });
+
+  document.querySelector('.menu-close').addEventListener('click', function () {
+    slideout.close();
+  });
+
+  function close(eve) {
+    eve.preventDefault();
+    slideout.close();
+  }
+
+  // Затемнение сайта
+  slideout
+    .on('beforeopen', function () {
+      this.panel.classList.add('panel-open');
+    })
+    .on('open', function () {
+      this.panel.addEventListener('click', close);
+    })
+    .on('beforeclose', function () {
+      this.panel.classList.remove('panel-open');
+      this.panel.removeEventListener('click', close);
+    });
+
+  slideout.on('open', function () {
+    $('#btn').addClass('on');
+  });
+
+  slideout.on('close', function () {
+    $('#btn').removeClass('on');
+  });
+
+
+  // Переворот иконок
+  $(".question__content, .lng, .header__lng").click(function () {
+    $(this).toggleClass("active");
+  });
+
+  // Таймер часовой
+  $('.jackpot__block__timer_clock1, .lottery__rally__timer1').timeTo(new Date('Wed Nov 22 2017 02:00:00 GMT+0200 (EET)'))
+  $('.jackpot__block__timer_clock2, .lottery__rally__timer2').timeTo(new Date('Wed Nov 22 2017 02:00:00 GMT+0200 (EET)'))
+  $('.jackpot__block__timer_clock3').timeTo(new Date('Wed Nov 22 2017 16:00:00 GMT+0200 (EET)'))
+
+  // Таймер дневной
+  var d = [5, 4, 3, 2, 1, 7, 6];
+  var today = new Date();
+  var end = new Date(today.getFullYear(), today.getMonth(), today.getDate() + d[today.getDay()], 16, 00, 00);
+  var _second = 1000;
+  var _minute = _second * 60;
+  var _hour = _minute * 60;
+  var _day = _hour * 24;
+  var timer;
+
+  function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+      return;
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    if (hours < 10) hours = '0' + hours;
+    var minutes = Math.floor((distance % _hour) / _minute);
+    if (minutes < 10) minutes = '0' + minutes;
+    var seconds = Math.floor((distance % _minute) / _second);
+    if (seconds < 10) seconds = '0' + seconds;
+    $('.lottery__rally__timer3--color span').text(days);
+    $('.timer_hou').text(hours);
+    $('.timer_minut').text(minutes);
+    $('.timer_secon').text(seconds);
+  }
+
+  timer = setInterval(showRemaining, 1000);
+
+  // Параллакс
+  var scene = $('.scene').get(0);
+  var parallaxInstance = new Parallax(scene);
+});
+
+var myCounter = new flipCounter('flip-counter', { value: 10000, inc: 123, pace: 600, auto: true });
+var myCounter = new flipCounter('flip-counter1', { value: 10000, inc: 123, pace: 600, auto: true });
+var myCounter = new flipCounter('flip-counter2', { value: 10000, inc: 123, pace: 600, auto: true });
---
+++ app/index.js
@@ -0,0 +1,65 @@
+// Requires
+const path = require('path');
+const express = require('express');
+const popsicle = require('popsicle');
+const fs = require('fs');
+
+// Server setup
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+    
+// Functions for API auth
+function getClientConfig() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('config.json', (error, config) => {
+      if(error) {reject(error)}
+      else {resolve(JSON.parse(config))}
+    })
+  })
+}
+
+function getAuthToken(config) {
+  return new Promise((resolve, reject) => {
+    popsicle.request({
+      method: 'POST',
+      url: config.url,
+      body: {
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        grant_type: 'client_credentials'
+      }
+    })
+    .use(popsicle.plugins.parse('json'))
+    .then((response) => {
+      if(response.status == 200) {
+        resolve(response.body.access_token);
+      }
+      else {
+        reject(`[${response.status}] ${response.statusText}`);
+      }
+    })
+    .catch((error) => {
+      reject(`[${error.code}] ${error}`);
+    });
+  });
+}
+
+// Routes
+app.get('/', (request, response) => {
+  response.render('index')
+});
+
+app.get('/token/', (request, response) => {
+  getClientConfig()
+    .then(getAuthToken)
+    .then((result) => {response.send(result)})
+    .catch((error) => {response.send(error)})  
+});
+
+// Start server
+app.listen(port);
+console.log(`Server listening to port ${port}`);
